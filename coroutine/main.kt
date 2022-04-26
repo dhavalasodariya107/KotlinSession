c43f80181b356d350ae37bfb299aaa3dc7b6841a
Dhaval Asodariya <94097838+dhavalasodariya107@users.noreply.github.com>
2022-04-26 17:45:04 +0530

    Update main.kt

--- coroutine/main.kt
+++ coroutine/main.kt
@@ -3,14 +3,15 @@ import kotlinx.coroutines.runBlocking
 fun main(args: Array<String>) {
     runBlocking {
         val tesla = Tesla()
-        tesla.build(10) { duration ->
+        val numberOfCars = 10_00_000
+        tesla.build(numberOfCars) { result ->
             println("---------------------------")
             println()
             println(
-                "Total time taken: ${duration.first} milli-seconds"
+                "Total time taken to build ${result.totalCars} cars: ${result.totalTime} milli-seconds"
             )
             println(
-                "Single car took: ${duration.second} milli-seconds"
+                "Single car took: ${result.singleCardBuildTime} milli-seconds"
             )
             println()
             println("---------------------------")
