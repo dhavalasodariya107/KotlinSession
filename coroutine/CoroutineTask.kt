af1faeb3f1202e9ef4f9a71e2583ab4f4ae064f5
Dhaval Asodariya <94097838+dhavalasodariya107@users.noreply.github.com>
2022-04-26 17:44:42 +0530

    Update CoroutineTask.kt

--- coroutine/CoroutineTask.kt
+++ coroutine/CoroutineTask.kt
@@ -1,105 +1,79 @@
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
-import kotlin.time.TimedValue
-import kotlin.time.measureTimedValue
 
-abstract class Car {
-    suspend fun build(currentCar: String) {
-        println("👇👇👇👇👇👇👇👇👇👇👇")
-        println("$currentCar: Preparing body...")
-        val bodyDuration = measureTimeMillis { prepareBody() }
-        println("$currentCar: Body prepared(${bodyDuration.toSecondUnit()})")
-        println()
 
-        println("👇👇👇👇👇👇👇👇👇👇👇")
-        println("$currentCar: Painting body...")
-        val paintDuration = measureTimeMillis { paint() }
-        println("$currentCar: Painting done(${paintDuration.toSecondUnit()})")
-        println()
+data class Result(
+    val totalTime: Long,
+    val singleCardBuildTime: Long,
+    val totalCars: Int,
+)
 
-        println("👇👇👇👇👇👇👇👇👇👇👇")
-        println("$currentCar: Installing drive train...")
-        val dtDuration = measureTimeMillis { installDriveTrain() }
-        println("$currentCar: Drive train installed(${dtDuration.toSecondUnit()})")
-        println()
-
-        println("👇👇👇👇👇👇👇👇👇👇👇")
-        println("$currentCar: Installing batteries...")
-        val batteriesDuration = measureTimeMillis { installBatteries() }
-        println("$currentCar: Batteries installed(${batteriesDuration.toSecondUnit()})")
-        println()
-
-        println("👇👇👇👇👇👇👇👇👇👇👇")
-
-        println("$currentCar: Installing interiors...")
-        val interiorsDuration = measureTimeMillis { addInterior() }
-        println("$currentCar: Interior installed(${interiorsDuration.toSecondUnit()})")
-    }
-
-    protected abstract suspend fun prepareBody()
-    protected abstract suspend fun paint()
-    protected abstract suspend fun installDriveTrain()
-    protected abstract suspend fun installBatteries()
-    protected abstract suspend fun addInterior()
-}
-
-class Tesla : Car() {
+class Tesla {
 
     @OptIn(ExperimentalTime::class)
-    suspend fun build(numberOfCars: Int, onComplete: (Pair<Long, Long>) -> Unit) {
-        val arrayOfMillis = arrayListOf<Long>()
-        val totalDuration = measureTimeMillis {
-            coroutineScope {
-                val job = launch {
-                    repeat((1..numberOfCars).count()) {
-                        launch {
-                            val timeTaken = prepareCar(it, numberOfCars)
-                            arrayOfMillis.add(timeTaken)
-                        }
+    suspend fun build(numberOfCars: Int, onComplete: (Result) -> Unit) {
+        var carBuild = 0
+        var startTime = 0L
+        var endTime = 0L
+        val arrayOfTime = mutableListOf<Long>()
+        coroutineScope {
+            val buildJob = launch {
+                startTime = System.currentTimeMillis()
+                println("Building cars...")
+                repeat((1..numberOfCars).count()) {
+                    launch {
+                        arrayOfTime.add(measureTimeMillis {
+                            carBuild = prepareCar(it)
+                        })
                     }
                 }
-                job.join()
+            }
+
+            val timerTask = launch {
+                var index = 1
+                while (isActive) {
+                    delay(1000)
+                    println("$index")
+                    index++
+                }
+            }
+
+            val cancellationJob = launch {
+                delay(27.seconds.inWholeMilliseconds)
+                buildJob.cancel("❌❌❌❌❌❌❌ TIME UP ❌❌❌❌❌❌❌")
+            }
+
+            buildJob.invokeOnCompletion {
+                cancellationJob.cancel()
+                timerTask.cancel()
+                it?.message?.let { msg -> println(msg) }
+                endTime = System.currentTimeMillis()
+                val totalTime = endTime - startTime
+                onComplete.invoke(
+                    Result(
+                        totalTime,
+                        arrayOfTime.average().toLong(),
+                        carBuild
+                    )
+                )
             }
         }
-
-//        val singleCarDuration = arrayOfMillis.sum() / arrayOfMillis.count().toLong()
-        val singleCarDuration = arrayOfMillis.average().toLong()
-        onComplete.invoke(Pair(totalDuration, (singleCarDuration)))
     }
 
-    private suspend fun prepareCar(currentCar: Int, numberOfCars: Int): Long {
-        return measureTimeMillis {
-            super.build("Car ${currentCar + 1}")
-            println("${currentCar + 1}/$numberOfCars ready for delivery")
-            println("🚗".repeat(currentCar + 1))
-            println()
-        }
-    }
-
-    override suspend fun prepareBody() {
+    private suspend fun prepareCar(currentCar: Int): Int {
         delay(5.toSecond())
-    }
-
-    override suspend fun paint() {
         delay(5.toSecond())
-    }
-
-    override suspend fun installDriveTrain() {
         delay(10.toSecond())
-    }
-
-    override suspend fun installBatteries() {
         delay(15.toSecond())
-    }
-
-    override suspend fun addInterior() {
         delay(15.toSecond())
+        return currentCar + 1
     }
 }
 
 fun Int.toSecond(): Long {
-    return this * 1000L
+    return this * 1000L / 2
 }
 
 fun Long.toSecondUnit(): String {
